2ab1f8ff88868b37f335384437bb01dc130c506b
Evgeniya Karpunina <janekarpunina@gmail.com>
2026-03-04 16:55:10 +0400

    Added spring configurations, application.properties, web.xml and schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+
+create table if not exists post(
+  id int AUTO_INCREMENT primary key,
+  title varchar(256) not null,
+  text CLOB not null,
+  likesCount integer not null);
+
+create table if not exists post_comment(
+  id int AUTO_INCREMENT primary key,
+  text CLOB not null,
+  post_id int,
+  CONSTRAINT fk_post FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE);
+
+create table if not exists tag(
+  id int AUTO_INCREMENT primary key,
+  name varchar(256) not null);
+
+
+create table if not exists post_tags(
+  tag_id int,
+  post_id int,
+  CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE CASCADE,
+  CONSTRAINT fk_post FOREIGN KEY (post_id) REFERENCES post(id) ON DELETE CASCADE);
+
+
